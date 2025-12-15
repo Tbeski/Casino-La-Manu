59aba3c0543f66027909157fbbe3965c8a91d29c
Tbeski <102597804+Tbeski@users.noreply.github.com>
2025-12-15 13:11:11 +0100

    update 15/12

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,38 @@
-// Variables Globales
+//VARIABLES GLOBALES
 let cash = 200;
 let bet = 0;
 let lst_cards = ["As ♥", "Roi ♥", "Dame ♥", "Valet ♥", "2 ♥", "3 ♥", "4 ♥", "5 ♥", "6 ♥", "7 ♥", "8 ♥", "9 ♥", "10 ♥",
                 "As ♦", "Roi ♦", "Dame ♦", "Valet ♦", "2 ♦", "3 ♦", "4 ♦", "5 ♦", "6 ♦", "7 ♦", "8 ♦", "9 ♦", "10 ♦",
                 "As ♣", "Roi ♣", "Dame ♣", "Valet ♣", "2 ♣", "3 ♣", "4 ♣", "5 ♣", "6 ♣", "7 ♣", "8 ♣", "9 ♣", "10 ♣",
                 "As ♠", "Roi ♠", "Dame ♠", "Valet ♠", "2 ♠", "3 ♠", "4 ♠", "5 ♠", "6 ♠", "7 ♠", "8 ♠", "9 ♠", "10 ♠"];
+
+
+const soldeCourant = document.querySelector("#soldeCourant");
+const inputMise = document.querySelector(".mise input");
+const btnMiser = document.querySelector(".miser");
+
+soldeCourant.textContent = cash;
+btnMiser.addEventListener("click", function(){
+    let valeurMise = parseInt(inputMise.value);
+    if(isNaN(valeurMise)){
+        alert("Entrez une mise valide")
+        return;
+    }
+    if (valeurMise < 20){
+        alert("Mise minimale : 20")
+        return;
+    }
+    if (valeurMise > cash){
+        alert("Solde insuffisant")
+        return;
+    }
+    bet = valeurMise;
+    cash -= bet;
+    soldeCourant.textContent = cash;
+    inputMise.value = "";
+});
+
+
 
 function initCards(){
     let deck = [...lst_cards];
@@ -46,91 +74,92 @@ function displayCards(name, cards, value){
 }
 
 function to_bet(cash){
-    let bet = parseInt(prompt("Entrez la mise souhaitée (Cash $" + cash + ") :")) //remplacer prompt par input
+    // let bet = parseInt(prompt("Entrez la mise souhaitée (Cash $" + cash + ") :")) //remplacer prompt par input
     while(isNaN(bet) || bet > cash || bet <20){
-        bet = prompt("Entrez une mise valide (cash $" + cash + ") :")//remplacer prompt par le bon input
+        // bet = prompt("Entrez une mise valide (cash $" + cash + ") :")//remplacer prompt par le bon input
     }
     return bet //cash est déjà géré à l'appel de la fonction
 }
 
 
-while (true){
-    if(cash < 20){
-        console.log("Vous n'avez plus assez de cash pour miser. Fin de la partie.")
-        break
-    }
+// while (true){
+//     if(cash < 20){
+//         console.log("Vous n'avez plus assez de cash pour miser. Fin de la partie.")
+//         break
+//     }
 
-    let deck = initCards()
-    let bet = to_bet(cash)
-    cash -= bet
-    let playerCards = [] //comprendre erreur
-    let dealerCards = []
-    let perdu = false
+//     let deck = initCards()
+//     let bet = to_bet(cash)
+//     cash -= bet
+//     let playerCards = [] //comprendre erreur
+//     let dealerCards = []
+//     let perdu = false
 
-    playerCards.push(draw(deck))
-    dealerCards.push(draw(deck))
-    playerCards.push(draw(deck))
+//     playerCards.push(draw(deck))
+//     dealerCards.push(draw(deck))
+//     playerCards.push(draw(deck))
 
-    let card = draw(deck)
-    dealerCards.push(card)
+//     let card = draw(deck)
+//     dealerCards.push(card)
     
-    let valuePlayer = getValueCards(playerCards)
-    let valueDealer = getValueCards(dealerCards)
+//     let valuePlayer = getValueCards(playerCards)
+//     let valueDealer = getValueCards(dealerCards)
 
-    displayCards("Croupier " , dealerCards, valueDealer)
-    displayCards("Joueur " , playerCards, valuePlayer)
+//     displayCards("Croupier " , dealerCards, valueDealer)
+//     displayCards("Joueur " , playerCards, valuePlayer)
 
-    while (true){
-        let pioche = prompt("Voulez-vous piocher ? (o/n)") //remplacer prompt par input
-        while (!(["o", "oui", "n", "non"].includes(pioche.toLowerCase()))){
-            pioche = prompt("voulez vous piocher ? (o/n)")
-        }if (pioche.toLowerCase() == "n"){
-            break;
-        }
-        let card = draw(deck)
-        playerCards.push(card)
-        valuePlayer = getValueCards(playerCards)
-        console.log()//print
-        displayCards("Croupier " , dealerCards, valueDealer)
-        displayCards("Joueur " , playerCards, valuePlayer)
-        console.log()
-        if (valuePlayer > 21){
-            perdu = true
-            break
-        }
-    }
+//     while (true){
+//         let pioche = prompt("Voulez-vous piocher ? (o/n)") //remplacer prompt par input
+//         while (!(["o", "oui", "n", "non"].includes(pioche.toLowerCase()))){
+//             pioche = prompt("voulez vous piocher ? (o/n)")
+//         }if (pioche.toLowerCase() == "n"){
+//             break;
+//         }
+//         let card = draw(deck)
+//         playerCards.push(card)
+//         valuePlayer = getValueCards(playerCards)
+//         console.log()//print
+//         displayCards("Croupier " , dealerCards, valueDealer)
+//         displayCards("Joueur " , playerCards, valuePlayer)
+//         console.log()
+//         if (valuePlayer > 21){
+//             perdu = true
+//             break
+//         }
+//     }
 
-    if (!perdu){
-        while (valueDealer <= valuePlayer && valueDealer < 21){
-            let card = draw(deck)
-            dealerCards.push(card)
-            valueDealer = getValueCards(dealerCards)
-        }
-        console.log()
-        displayCards("Croupier" , dealerCards, valueDealer)
-        displayCards("Joueur" , playerCards, valuePlayer)
-        console.log()
-    }
-    if (valuePlayer > 21){
-        console.log("Vous avez perdu !")
-    }else if (valueDealer > 21){
-        console.log("Vous avez gagné ! $" + bet * 2 + "!")
-        cash += bet * 2
-    }else if (valueDealer == valuePlayer){
-        console.log("Egalité !")
-        cash += bet
-    }else if (valueDealer > valuePlayer){
-        console.log("Vous avez perdu !")
-    }else{
-        console.log("Vous avez gagné" + bet*2 +" !")
-        cash += bet * 2
-    }
+//     if (!perdu){
+//         while (valueDealer <= valuePlayer && valueDealer < 21){
+//             let card = draw(deck)
+//             dealerCards.push(card)
+//             valueDealer = getValueCards(dealerCards)
+//         }
+//         console.log()
+//         displayCards("Croupier" , dealerCards, valueDealer)
+//         displayCards("Joueur" , playerCards, valuePlayer)
+//         console.log()
+//     }
+//     if (valuePlayer > 21){
+//         console.log("Vous avez perdu !")
+//     }else if (valueDealer > 21){
+//         console.log("Vous avez gagné $" + bet * 2 + "!")
+//         cash += bet * 2
+//     }else if (valueDealer == valuePlayer){
+//         console.log("Egalité !")
+//         cash += bet
+//     }else if (valueDealer > valuePlayer){
+//         console.log("Vous avez perdu !")
+//     }else{
+//         console.log("Vous avez gagné" + bet*2 +" !")
+//         cash += bet * 2
+//     }
 
-    let quitGame = prompt("Voulez vous continuer la partie ? (o/n)")
-    while (!(["o", "n", "oui", "non"].includes(quitGame.toLowerCase()))){
-        quitGame = prompt("Voulez vous continuer la partie ? (o/n)")
-    }
-    if (quitGame.toLowerCase() == "n"){
-        break
-    }
-}
+//     // let quitGame = prompt("Voulez vous continuer la partie ? (o/n)")
+//     while (!(["o", "n", "oui", "non"].includes(quitGame.toLowerCase()))){
+//         // quitGame = prompt("Voulez vous continuer la partie ? (o/n)")
+//     }
+//     if (quitGame.toLowerCase() == "n"){
+//         break
+//     }
+// }
+
