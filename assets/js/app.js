49ff5f7496a0c7e58cd12ac4a6cb9bd06dbd60d3
Tbeski <102597804+Tbeski@users.noreply.github.com>
2025-12-17 16:00:27 +0100

    update 17/12

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,31 @@
+/*
+|PARKING DES CURSEURS
+|Tbesh :
+________________
+|Teddy :        \
+________________|
+*/
+
 //VARIABLES GLOBALES
+let deck = [];
+let playerCards = [];
+let dealerCards = [];
+let valuePlayer = 0;
+let valueDealer = 0;
+
 let cash = 200;
 let bet = 0;
-let lst_cards = ["As ♥", "Roi ♥", "Dame ♥", "Valet ♥", "2 ♥", "3 ♥", "4 ♥", "5 ♥", "6 ♥", "7 ♥", "8 ♥", "9 ♥", "10 ♥",
-                "As ♦", "Roi ♦", "Dame ♦", "Valet ♦", "2 ♦", "3 ♦", "4 ♦", "5 ♦", "6 ♦", "7 ♦", "8 ♦", "9 ♦", "10 ♦",
-                "As ♣", "Roi ♣", "Dame ♣", "Valet ♣", "2 ♣", "3 ♣", "4 ♣", "5 ♣", "6 ♣", "7 ♣", "8 ♣", "9 ♣", "10 ♣",
-                "As ♠", "Roi ♠", "Dame ♠", "Valet ♠", "2 ♠", "3 ♠", "4 ♠", "5 ♠", "6 ♠", "7 ♠", "8 ♠", "9 ♠", "10 ♠"];
+let lst_cards = ["As coeur", "Roi coeur", "Dame coeur", "Valet coeur", "2 coeur", "3 coeur", "4 coeur", "5 coeur", "6 coeur", "7 coeur", "8 coeur", "9 coeur", "10 coeur",
+                "As carreau", "Roi carreau", "Dame carreau", "Valet carreau", "2 carreau", "3 carreau", "4 carreau", "5 carreau", "6 carreau", "7 carreau", "8 carreau", "9 carreau", "10 carreau",
+                "As trefle", "Roi trefle", "Dame trefle", "Valet trefle", "2 trefle", "3 trefle", "4 trefle", "5 trefle", "6 trefle", "7 trefle", "8 trefle", "9 trefle", "10 trefle",
+                "As pique", "Roi pique", "Dame pique", "Valet pique", "2 pique", "3 pique", "4 pique", "5 pique", "6 pique", "7 pique", "8 pique", "9 pique", "10 pique"];
 
-
-const soldeCourant = document.querySelector("#soldeCourant");
+const soldeCourant = document.querySelectorAll(".soldeCourant");
 const inputMise = document.querySelector(".mise input");
 const btnMiser = document.querySelector(".miser");
-
-soldeCourant.textContent = cash;
-btnMiser.addEventListener("click", function(){
-    let valeurMise = parseInt(inputMise.value);
-    if(isNaN(valeurMise)){
-        alert("Entrez une mise valide")
-        return;
-    }
-    if (valeurMise < 20){
-        alert("Mise minimale : 20")
-        return;
-    }
-    if (valeurMise > cash){
-        alert("Solde insuffisant")
-        return;
-    }
-    bet = valeurMise;
-    cash -= bet;
-    soldeCourant.textContent = cash;
-    inputMise.value = "";
-});
-
+const miseActuelle = document.querySelector(".mise_actuelle");
+const btnpiocher = document.querySelector(".piocher");
+const cartes = document.querySelectorAll(".carte")
 
 
 function initCards(){
@@ -60,10 +54,10 @@ function getValueCards(cards){
             ace += 1;
     }
 
-    value += ace * 11
+    value += ace * 11;
     while(value > 21 && ace > 0){
-        value -= 10
-        ace -= 1
+        value -= 10;
+        ace -= 1;
     }
     return value
 }
@@ -73,14 +67,78 @@ function displayCards(name, cards, value){
     //TEMPORAIRE
 }
 
-function to_bet(cash){
-    // let bet = parseInt(prompt("Entrez la mise souhaitée (Cash $" + cash + ") :")) //remplacer prompt par input
-    while(isNaN(bet) || bet > cash || bet <20){
-        // bet = prompt("Entrez une mise valide (cash $" + cash + ") :")//remplacer prompt par le bon input
-    }
-    return bet //cash est déjà géré à l'appel de la fonction
+// function to_bet(cash){
+//     // let bet = parseInt(prompt("Entrez la mise souhaitée (Cash $" + cash + ") :")) //remplacer prompt par input
+//     while(isNaN(bet) || bet > cash || bet <20){
+//         // bet = prompt("Entrez une mise valide (cash $" + cash + ") :")//remplacer prompt par le bon input
+//     }
+//     return bet; //cash est déjà géré à l'appel de la fonction
+// }
+
+function cardToImage(card){
+    let [value, symbol] = card.split(" ");
+
+    if (card == "dos") return `assets/images/Cartes/Dos de la carte.svg`;
+    return `assets/images/Cartes/${value} ${symbol}.svg`
+}       
+
+function updateCards(joueur, carte, position){
+    let containerId = joueur == "player"?"#cartes_joueurs":"#cartes_croupier";
+    let container = document.querySelector(containerId);
+
+    let img = container.querySelector(`img[data-carte="${position}"]`);
+    if (!img) return;
+
+    img.src = cardToImage(carte);
+}
+for (e of soldeCourant){
+    e.textContent = cash;
 }
 
+btnMiser.addEventListener("click", function(){
+    let valeurMise = parseInt(inputMise.value);
+    if(isNaN(valeurMise)){
+        alert("Entrez une mise valide")
+        return;
+    }
+    if (valeurMise < 20){
+        alert("Mise minimale : 20")
+        return;
+    }
+    if (valeurMise > cash){
+        alert("Solde insuffisant")
+        return;
+    }
+    bet = valeurMise;
+    cash -= bet;
+    for (e of soldeCourant){
+    e.textContent = cash;
+    }
+    miseActuelle.textContent = valeurMise;
+    inputMise.value = "";
+    deck = initCards()
+    playerCards = [];
+    dealerCards = [];
+    playerCards.push(draw(deck));
+    updateCards("player", playerCards[0], 1);
+    dealerCards.push(draw(deck));
+    updateCards("dealer", dealerCards[0], 1);
+    playerCards.push(draw(deck));
+    updateCards("player", playerCards[1], 2);
+    dealerCards.push(draw(deck));
+    updateCards("dealer", "dos", 2);
+    valuePlayer = getValueCards(playerCards);
+    valueDealer = getValueCards(dealerCards);
+    displayCards("Joueur", playerCards, valuePlayer);
+    displayCards("Croupier", dealerCards, valueDealer);
+});
+
+btnpiocher.addEventListener("click", function(){
+    playerCards.push(draw(deck));
+    updateCards("player", playerCards[playerCards.length-1], playerCards.length);
+    valuePlayer = getValueCards(playerCards);
+    displayCards("Joueur", playerCards, valuePlayer);
+});
 
 // while (true){
 //     if(cash < 20){
@@ -162,4 +220,5 @@ function to_bet(cash){
 //         break
 //     }
 // }
+
 
