d3bc6e080a60186626531776a17ae76385d38bbf
Tbeski <102597804+Tbeski@users.noreply.github.com>
2025-12-18 16:17:09 +0100

    Version Rendue 18/12

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,3 @@
-/*
-|PARKING DES CURSEURS
-|Tbesh :
-________________
-|Teddy :        \
-________________|
-*/
-
 //VARIABLES GLOBALES
 let deck = [];
 let playerCards = [];
@@ -15,19 +7,25 @@ let valueDealer = 0;
 
 let cash = 200;
 let bet = 0;
-let lst_cards = ["As coeur", "Roi coeur", "Dame coeur", "Valet coeur", "2 coeur", "3 coeur", "4 coeur", "5 coeur", "6 coeur", "7 coeur", "8 coeur", "9 coeur", "10 coeur",
-                "As carreau", "Roi carreau", "Dame carreau", "Valet carreau", "2 carreau", "3 carreau", "4 carreau", "5 carreau", "6 carreau", "7 carreau", "8 carreau", "9 carreau", "10 carreau",
-                "As trefle", "Roi trefle", "Dame trefle", "Valet trefle", "2 trefle", "3 trefle", "4 trefle", "5 trefle", "6 trefle", "7 trefle", "8 trefle", "9 trefle", "10 trefle",
-                "As pique", "Roi pique", "Dame pique", "Valet pique", "2 pique", "3 pique", "4 pique", "5 pique", "6 pique", "7 pique", "8 pique", "9 pique", "10 pique"];
+let lst_cards = ["As coeur", "roi coeur", "dame coeur", "valet coeur", "2 coeur", "3 coeur", "4 coeur", "5 coeur", "6 coeur", "7 coeur", "8 coeur", "9 coeur", "10 coeur",
+                "As carreau", "roi carreau", "dame carreau", "valet carreau", "2 carreau", "3 carreau", "4 carreau", "5 carreau", "6 carreau", "7 carreau", "8 carreau", "9 carreau", "10 carreau",
+                "As trefle", "roi trefle", "dame trefle", "valet trefle", "2 trefle", "3 trefle", "4 trefle", "5 trefle", "6 trefle", "7 trefle", "8 trefle", "9 trefle", "10 trefle",
+                "As pique", "roi pique", "dame pique", "valet pique", "2 pique", "3 pique", "4 pique", "5 pique", "6 pique", "7 pique", "8 pique", "9 pique", "10 pique"];
 
 const soldeCourant = document.querySelectorAll(".soldeCourant");
 const inputMise = document.querySelector(".mise input");
 const btnMiser = document.querySelector(".miser");
 const miseActuelle = document.querySelector(".mise_actuelle");
-const btnpiocher = document.querySelector(".piocher");
+const btnPiocher = document.querySelector(".piocher");
+const btnRester = document.querySelector(".rester")
 const cartes = document.querySelectorAll(".carte")
+const boutons = document.querySelector(".boutons")
+const btnRejouer = document.querySelector(".rejouer")
+const totalPlayer = document.querySelector(".player_game .totalcartes span");
+const totalDealer = document.querySelector(".dealer_game .totalcartes span");
 
 
+console.log(cartes);
 function initCards(){
     let deck = [...lst_cards];
     for (let i = deck.length - 1; i > 0; i--) {
@@ -48,7 +46,7 @@ function getValueCards(cards){
         let v = card.split(" ")[0].trim(); //remplacé cards.split par card.split
         if (!isNaN(parseInt(v))) { //isdigit c en python, remplacé par isNan
             value += parseInt(v); //parseInt au lieu de int
-        }else if (["Valet", "Dame", "Roi"].includes(v)){ //tab.includes(valeur) en JS
+        }else if (["valet", "dame", "roi"].includes(v)){ //tab.includes(valeur) en JS
             value += 10;
         }else 
             ace += 1;
@@ -67,14 +65,6 @@ function displayCards(name, cards, value){
     //TEMPORAIRE
 }
 
-// function to_bet(cash){
-//     // let bet = parseInt(prompt("Entrez la mise souhaitée (Cash $" + cash + ") :")) //remplacer prompt par input
-//     while(isNaN(bet) || bet > cash || bet <20){
-//         // bet = prompt("Entrez une mise valide (cash $" + cash + ") :")//remplacer prompt par le bon input
-//     }
-//     return bet; //cash est déjà géré à l'appel de la fonction
-// }
-
 function cardToImage(card){
     let [value, symbol] = card.split(" ");
 
@@ -91,9 +81,89 @@ function updateCards(joueur, carte, position){
 
     img.src = cardToImage(carte);
 }
-for (e of soldeCourant){
-    e.textContent = cash;
+
+function updateTotal(){
+    totalDealer.textContent = valueDealer
+    totalPlayer.textContent = valuePlayer
 }
+
+function endRound() {
+
+    if (valueDealer > 21) {
+        cash += bet * 2;
+        alert("Le croupier bust ! Vous gagnez");
+        miseActuelle.textContent = "0";
+    }
+    else if (valueDealer === valuePlayer) {
+        cash += bet;
+        alert("Égalité");
+        miseActuelle.textContent = "0";
+    }
+    else {
+        alert("Vous perdez");
+       miseActuelle.textContent = "0";
+    }
+
+    for (let e of soldeCourant) {
+        e.textContent = cash;
+    }
+    btnRejouer.disabled = false;
+}
+
+function dealerPlay() {
+
+    if (valueDealer >= valuePlayer || valueDealer > 21) {
+        endRound();
+        return;
+    }
+
+    dealerCards.push(draw(deck));
+    updateCards(
+        "dealer",
+        dealerCards[dealerCards.length - 1],
+        dealerCards.length
+    );
+
+    valueDealer = getValueCards(dealerCards);
+    updateTotal();
+
+    setTimeout(dealerPlay, 800);
+}
+
+function resetRound() {
+    deck = [];
+    playerCards = [];
+    dealerCards = [];
+    valuePlayer = 0;
+    valueDealer = 0;
+    bet = 0;
+
+    miseActuelle.textContent = "0";
+
+    document.querySelectorAll("#cartes_joueurs img, #cartes_croupier img")
+        .forEach(img => img.src = "assets/images/Cartes/Rectangle 20.svg");
+
+    btnMiser.disabled = false;
+    btnPiocher.disabled = true;
+    btnRester.disabled = true;
+    btnRejouer.disabled = true;
+
+    inputMise.style.visibility = "visible";
+    btnMiser.style.visibility = "visible";
+
+    totalPlayer.textContent = "?";
+    totalDealer.textContent = "0";
+
+}
+
+
+btnMiser.disabled = false;
+btnPiocher.disabled = false;
+btnRejouer.disabled = true;
+
+soldeCourant.forEach(e => {
+    e.textContent = cash;
+});
 
 btnMiser.addEventListener("click", function(){
     let valeurMise = parseInt(inputMise.value);
@@ -109,11 +179,17 @@ btnMiser.addEventListener("click", function(){
         alert("Solde insuffisant")
         return;
     }
+
+    [inputMise, btnMiser].forEach(e => {
+        e.style.visibility = "hidden"
+    });
+    btnRester.disabled = false;
+    btnPiocher.disabled = false;
     bet = valeurMise;
     cash -= bet;
-    for (e of soldeCourant){
+    soldeCourant.forEach(e => {
     e.textContent = cash;
-    }
+    });
     miseActuelle.textContent = valeurMise;
     inputMise.value = "";
     deck = initCards()
@@ -129,96 +205,35 @@ btnMiser.addEventListener("click", function(){
     updateCards("dealer", "dos", 2);
     valuePlayer = getValueCards(playerCards);
     valueDealer = getValueCards(dealerCards);
-    displayCards("Joueur", playerCards, valuePlayer);
-    displayCards("Croupier", dealerCards, valueDealer);
+    totalPlayer.textContent = valuePlayer
+    totalDealer.textContent = "?"
 });
 
-btnpiocher.addEventListener("click", function(){
+btnPiocher.addEventListener("click", function(){
     playerCards.push(draw(deck));
     updateCards("player", playerCards[playerCards.length-1], playerCards.length);
     valuePlayer = getValueCards(playerCards);
-    displayCards("Joueur", playerCards, valuePlayer);
+    totalPlayer.textContent = valuePlayer
+    if(valuePlayer>21){
+        btnPiocher.disabled = true;
+        btnRester.disabled = true;
+        updateCards("dealer", dealerCards[1], 2);
+        updateTotal();
+        endRound();
+    }
 });
 
-// while (true){
-//     if(cash < 20){
-//         console.log("Vous n'avez plus assez de cash pour miser. Fin de la partie.")
-//         break
-//     }
+btnRester.addEventListener("click", function () {
+    btnPiocher.disabled = true;
+    btnRester.disabled = true;
+    updateCards("dealer", dealerCards[1],2)
+    valueDealer = getValueCards(dealerCards)
+    updateTotal();
+    dealerPlay();
+});
 
-//     let deck = initCards()
-//     let bet = to_bet(cash)
-//     cash -= bet
-//     let playerCards = [] //comprendre erreur
-//     let dealerCards = []
-//     let perdu = false
-
-//     playerCards.push(draw(deck))
-//     dealerCards.push(draw(deck))
-//     playerCards.push(draw(deck))
-
-//     let card = draw(deck)
-//     dealerCards.push(card)
-    
-//     let valuePlayer = getValueCards(playerCards)
-//     let valueDealer = getValueCards(dealerCards)
-
-//     displayCards("Croupier " , dealerCards, valueDealer)
-//     displayCards("Joueur " , playerCards, valuePlayer)
-
-//     while (true){
-//         let pioche = prompt("Voulez-vous piocher ? (o/n)") //remplacer prompt par input
-//         while (!(["o", "oui", "n", "non"].includes(pioche.toLowerCase()))){
-//             pioche = prompt("voulez vous piocher ? (o/n)")
-//         }if (pioche.toLowerCase() == "n"){
-//             break;
-//         }
-//         let card = draw(deck)
-//         playerCards.push(card)
-//         valuePlayer = getValueCards(playerCards)
-//         console.log()//print
-//         displayCards("Croupier " , dealerCards, valueDealer)
-//         displayCards("Joueur " , playerCards, valuePlayer)
-//         console.log()
-//         if (valuePlayer > 21){
-//             perdu = true
-//             break
-//         }
-//     }
-
-//     if (!perdu){
-//         while (valueDealer <= valuePlayer && valueDealer < 21){
-//             let card = draw(deck)
-//             dealerCards.push(card)
-//             valueDealer = getValueCards(dealerCards)
-//         }
-//         console.log()
-//         displayCards("Croupier" , dealerCards, valueDealer)
-//         displayCards("Joueur" , playerCards, valuePlayer)
-//         console.log()
-//     }
-//     if (valuePlayer > 21){
-//         console.log("Vous avez perdu !")
-//     }else if (valueDealer > 21){
-//         console.log("Vous avez gagné $" + bet * 2 + "!")
-//         cash += bet * 2
-//     }else if (valueDealer == valuePlayer){
-//         console.log("Egalité !")
-//         cash += bet
-//     }else if (valueDealer > valuePlayer){
-//         console.log("Vous avez perdu !")
-//     }else{
-//         console.log("Vous avez gagné" + bet*2 +" !")
-//         cash += bet * 2
-//     }
-
-//     // let quitGame = prompt("Voulez vous continuer la partie ? (o/n)")
-//     while (!(["o", "n", "oui", "non"].includes(quitGame.toLowerCase()))){
-//         // quitGame = prompt("Voulez vous continuer la partie ? (o/n)")
-//     }
-//     if (quitGame.toLowerCase() == "n"){
-//         break
-//     }
-// }
-
-
+btnRejouer.addEventListener("click", function () {
+    resetRound();
+    totalDealer.textContent = "?"
+    totalPlayer.textContent = "0"
+});
